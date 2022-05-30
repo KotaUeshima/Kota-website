1d178c171f813bb4b306502698495de23e449c1a
Kota Ueshima <ueshima@usc.edu>
2022-05-29 22:20:14 -0700

    Content

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
